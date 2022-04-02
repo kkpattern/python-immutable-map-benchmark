86680dd6744f073ad0bb1c9b99ac27f518a03016
gzzhangkai2014 <gzzhangkai2014@corp.netease.com>
2022-04-02 21:29:57 +0800

    Add basic update and diff case

--- benchmark.py
+++ benchmark.py
@@ -113,6 +113,83 @@ def benchmark_remove(input_map):
     # print(f"builtin dict cost {dict_cost}")
 
 
+def update_and_diff_pyrsistent(map_,
+                               id_to_update,
+                               key_to_update,
+                               value_to_update):
+    new_object = map_[id_to_update].set(key_to_update, value_to_update)
+    new_map = map_.set(id_to_update, new_object)
+
+    dirty_objects = {}
+    for id_, object_ in new_map.items():
+        if object_ is not map_.get(id_, None):
+            dirty_objects[id_] = object_
+
+
+def update_and_diff_frozendict(map_,
+                               id_to_update,
+                               key_to_update,
+                               value_to_update):
+    new_object = map_[id_to_update].set(key_to_update, value_to_update)
+    new_map = map_.set(id_to_update, new_object)
+
+    dirty_objects = {}
+    for id_, object_ in new_map.items():
+        if object_ is not map_.get(id_, None):
+            dirty_objects[id_] = object_
+
+
+def update_and_diff_immutables(map_,
+                               id_to_update,
+                               key_to_update,
+                               value_to_update):
+    new_object = map_[id_to_update].set(key_to_update, value_to_update)
+    new_map = map_.set(id_to_update, new_object)
+
+    dirty_objects = {}
+    for id_, object_ in new_map.items():
+        if object_ is not map_.get(id_, None):
+            dirty_objects[id_] = object_
+
+
+def update_and_diff_case():
+    print("Update and diff")
+    benchmark_globals = globals()
+
+    id_to_object_raw = {}
+    for i in range(10000):
+        id_to_object_raw[i] = pmap({"name": f"name{i}"})
+    input_map = pmap(id_to_object_raw)
+    benchmark_globals["input_map"] = input_map
+    pyrsistent_cost = timeit.timeit(
+        "update_and_diff_pyrsistent(input_map, 1, \"name\", \"new_name\")",
+        number=100,
+        globals=benchmark_globals)
+    print(f"pyrsistent cost {pyrsistent_cost}")
+
+    id_to_object_raw = {}
+    for i in range(10000):
+        id_to_object_raw[i] = frozendict({"name": f"name{i}"})
+    input_map = frozendict(id_to_object_raw)
+    benchmark_globals["input_map"] = input_map
+    frozendict_cost = timeit.timeit(
+        "update_and_diff_frozendict(input_map, 1, \"name\", \"new_name\")",
+        number=100,
+        globals=benchmark_globals)
+    print(f"frozendict cost {frozendict_cost}")
+
+    id_to_object_raw = {}
+    for i in range(10000):
+        id_to_object_raw[i] = Map({"name": f"name{i}"})
+    input_map = Map(id_to_object_raw)
+    benchmark_globals["input_map"] = input_map
+    immutables_cost = timeit.timeit(
+        "update_and_diff_immutables(input_map, 1, \"name\", \"new_name\")",
+        number=100,
+        globals=benchmark_globals)
+    print(f"immutables cost {immutables_cost}")
+
+
 def main():
     for case in [benchmark_access, benchmark_set, benchmark_remove]:
         for size in [10, 100, 1000, 10000, 100000]:
@@ -120,6 +197,7 @@ def main():
             for i in range(size):
                 input_map[f"key{i}"] = i
             case(input_map)
+    update_and_diff_case()
 
 
 if __name__ == "__main__":
