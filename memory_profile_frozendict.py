7a178f10900fac4c171f75716854557db0a78033
gzzhangkai2014 <gzzhangkai2014@corp.netease.com>
2022-04-03 19:55:40 +0800

    Add memory profile

---
+++ memory_profile_frozendict.py
@@ -0,0 +1,18 @@
+from frozendict import frozendict
+from memory_profiler import profile
+
+
+@profile
+def main(input_map):
+    map_ = frozendict(input_map)
+    history = []
+    for i in range(1000):
+        history.append(map_)
+        map_ = map_.set(i, 0)
+
+
+if __name__ == "__main__":
+    input_map = {}
+    for i in range(1000):
+        input_map[i] = i
+    main(input_map)
